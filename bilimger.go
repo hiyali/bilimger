a36194c21ab4ca48ef3daecf43430636bbf69c33
Salam Hiyali <hiyali920@gmail.com>
2017-09-12 19:19:39 +0800

    Initial from zero-level

---
+++ bilimger.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "github.com/hiyali/bilimger/world"
+)
+
+func main () {
+  fmt.Println("hello " + world.Voice())
+}
---
+++ world/city.go
@@ -0,0 +1,7 @@
+package world
+
+import "strconv"
+
+func Voice () string {
+  return "city No." + strconv.Itoa(5)
+}
